31c9db2261bd54d80467a810f6445aef932d4c85
DKBARB10 <dkbarb10@localhost.localdomain>
2021-09-28 01:44:08 -0500

    get_command.c

--- get_command.c
+++ get_command.c
@@ -5,7 +5,8 @@ int get_cmd(char *cmd);
 int get_cmd(char *cmd)
 {
     system(cmd);
-
     if (strcmp(cmd, "exit") == 0)
         _exit_err();
+    else if (strcmp(cmd, "Chat_Hack") == 0)
+        _chat_hacking();
 }
